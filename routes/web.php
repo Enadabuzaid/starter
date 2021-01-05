59d0def8648ef8fae742f4dbbc0ad6fd796aff24
enadabuzaid <enad.abuzaid15@gmail.com>
2021-01-05 10:22:55 +0200

    =first commit

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+   return view('welcome');
+    //echo "Heloo Word";
+});
+
+Route::get('/test1', function (){
+   return 'welcome';
+});
+
+//route paramters
+Route::get('/test2/{id}', function ($id){
+    return $id;
+});
+
+Route::get('/test3/{id?}', function (){
+    return 'welcome';
+});
+
+/*
+Route::get('/show-number/{id}', function ($id){
+    return $id;
+})->name('a');
+
+Route::get('/show-string/{id?}', function (){
+    return 'welcome';
+});
+
+//rout name
+
+
+}
+
+// all route only access controller or methods in folder name Front
+  Route::get('/aboutme',function (){
+      return view("About");
+  });*/
+
